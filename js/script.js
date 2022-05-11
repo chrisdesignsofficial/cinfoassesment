c061f05b4000706622ee82bc3526a53c6d3770e8
Chris Nyeche <91467592+chrisdesignsofficial@users.noreply.github.com>
2022-05-11 15:25:40 +0100

    table data commit

--- js/script.js
+++ js/script.js
@@ -1,27 +1,77 @@
-// Syntax $ is to define, ("#.") the next thing is the selecctor 
-/*(#el).click(function(){
-    action goes here
-    =>Other events are dblclick, hover, hide, show, fadein, fadeout.
-    text() is for textContent
-    html()is for InnerHtml
+let form = $("#form");
+let firstName = $("#firstname")
+let firstNameInput = $("#firstNameInput")
+let lastname = $("#lastname")
+let lastnameInput =
+
+
+form.submit(function(){
+    firstNameInput.text(firstName.val())
 })
-*/
-let count = 0  
-let para =  $("#para");
-let input = $("#text")
-let test = $("#test")
-let list = $(".list")
-let btn =  $("button")
 
-    input.focus(() =>{
-        // input.css("background-color", "lightblue");
-        input.toggleClass("border border-danger")
-    });
+form.validate({
+rules:{
+    firstname:{
+      required: true, 
+    },
+    lastname:{
+        required: true, 
+    },
+    middlename:{
+        required: true, 
+    },
+    dob:{
+        required: true, 
+    },
+    sex:{
+        required: true, 
+    },
+    phone:{
+        required: true, 
+    },
+    country:{
+        required: true, 
+    },
+    state:{
+        required: true, 
+    },
+    lga:{
+        required: true, 
+    }
+},
+messages:{
+    firstname:{
+        required: "Please enter your legal first name", 
+      }, 
+      lastname:{
+        required: "Please enter your legal last name", 
+      },
+      middlename:{
+        required: "Please enter your legal middle name", 
+      }, 
+      dob:{
+        required: "Please enter your Date of Birth", 
+      },
+      sex:{
+        required: "Please enter your gender", 
+      },
+      phone:{
+        required: "Please enter your phone number", 
+      },
+      state:{
+        required: "Please enter your state of origin", 
+      },
+      lga:{
+        required: "Please enter your local government area", 
+      },
+}
+})
 
-   btn.click(function(){
-        console.log(input.val())
-        para.text(input.val())
-        list.empty()
-        btn.toggleClass("bg-success")
-    });
+// DataTable
+$('#table_id').DataTable({
 
+    // to limit records
+    pageLength: 4,
+
+    recordsTotal: 50,
+  });
